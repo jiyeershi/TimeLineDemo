3bc0d97afba132d33f3cfbcba765bb2a5ad9818a
liuyi <liuyi@fotoable.com>
2019-12-16 20:24:40 +0800

    更新

--- Assets/Scripts/RoleController.cs
+++ Assets/Scripts/RoleController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
+using UnityEngine.EventSystems;
 
-public class RoleController : MonoBehaviour
+public class RoleController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
 
     private NavMeshAgent man;
@@ -15,9 +17,37 @@ public class RoleController : MonoBehaviour
         man = GetComponent<NavMeshAgent>();
     }
 
+    public int ClickBtnFlag = 0;
+
     // Update is called once per frame
     void Update()
     {
         man.SetDestination(target.position);
+        bool isClickLeft = Input.GetMouseButtonDown(ClickBtnFlag);
+        if (isClickLeft)
+        {
+            //Debug.Log("Click mouse on : " + ClickBtnFlag);
+        }
+    }
+
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            
+        }else if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            
+        }
+        
+        Debug.Log("Click Mouse Down: " + eventData.button);
+        
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("Click Mouse Up: " + eventData.button);
     }
 }
